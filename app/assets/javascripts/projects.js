ad716967810d96cc5cc4ddf30a76ada83f849ded
gvaquez <gilles_vaquez@yahoo.fr>
2016-02-27 16:34:23 +0100

    test fix size

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,4 +1,25 @@
 
+Masonry.prototype._getMeasurement = function( measurement, size ) {
+  var option = this.options[ measurement ];
+  var elem;
+  if ( !option ) {
+    // default to 0
+    this[ measurement ] = 0;
+  } else if ( typeof option === 'function' ) {
+    this[ measurement ] = option.call( this );
+  } else {
+    // use option as an element
+    if ( typeof option === 'string' ) {
+      elem = this.element.querySelector( option );
+    } else if ( isElement( option ) ) {
+      elem = option;
+    }
+    // use size of element, if element
+    this[ measurement ] = elem ? getSize( elem )[ size ] : option;
+  }
+};
+
+
 $( document ).ready(function() {
     console.log( "ready!" );
     $(".grid").imagesLoaded( function() {
@@ -6,7 +27,17 @@ $( document ).ready(function() {
 	    	  // options
 	    	  itemSelector: '.grid-item',
 	    	  gutter: '.gutter-sizer',
-	    	  columnWidth: '.grid-sizer'
+	    	  //columnWidth: '.grid-sizer'
+	    	  columnWidth: function() {
+	    	      // 5 columns
+	    	      return 16;
+	    	    }
 	    	});
     });
+    
+    $('body').on('click', 'span[data-pin-log="button_pinit_floating"]', function(event) { 
+            ga('send', 'social', 'pinterest', 'pin', event.target.getAttribute('data-pin-href')); 
+        });
 });
+
+
